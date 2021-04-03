62c8809b746f8297e94537af2052be21ab74fa1c
Julia Assad <jassadsimao@netemedia.fr>
2021-04-03 15:02:14 +0200

    #4: colors in tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        container: {
+            center: true,
+        },
+        fontFamily: {
+
+        },
+        extend: {
+            colors: {
+                'quick-silver': '#97afa3',
+                'azure': '#f0f2f2',
+                'tart-orange' : '#f7484e',
+                'japanese-laurel' : '#2e7e32'
+            }
+        }
+    },
+}
