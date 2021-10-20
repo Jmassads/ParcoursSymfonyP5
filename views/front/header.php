241c9bc9b204d88caff1cb232f124e5a7a01d5c7
Julia Assad <jassadsimao@netemedia.fr>
2021-10-20 21:49:28 +0200

    site.webmanifest

--- views/front/header.php
+++ views/front/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>starter-2020</title>
     <link rel="shortcut icon" type="image/ico" href="<?php echo URL ?>public/favicon.ico">
-    <link rel="manifest" href="manifest.json">
+    <link rel="manifest" href="site.webmanifest">
 
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
