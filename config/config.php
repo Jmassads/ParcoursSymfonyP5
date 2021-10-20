c7b089a704d6f760c812f58758aa14c9ee9b92e3
Julia Assad <jassadsimao@netemedia.fr>
2021-10-20 23:58:57 +0200

    dernieres modification to fix symfony insight errors

--- config/config.php
+++ config/config.php
@@ -8,4 +8,3 @@ const PASSWORD = "root";
 const COOKIE_PROTECT = "timer";
 
 define("URL",str_replace("index.php","", (isset($_SERVER["HTTPS"])? "https" : "http"). "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
-
